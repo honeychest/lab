347dac05a13a0cd1a7154c8f78c16f925b8d7ed7
honeychest <scollo1982@gmail.com>
2026-02-26 00:01:59 +0900

    헤더 푸터 설정완료

---
+++ frontend/src/pages/BinancePage.jsx
@@ -0,0 +1,14 @@
+// Purpose: Binance 페이지 - 준비 중 안내 화면
+import Layout from '../layout/Layout.jsx';
+
+function BinancePage() {
+    return (
+        <Layout footerCenter={[]}>
+            <div style={{ padding: '80px', textAlign: 'center', fontSize: '24px', color: '#4a6fa5' }}>
+                준비중입니다.
+            </div>
+        </Layout>
+    );
+}
+
+export default BinancePage;
