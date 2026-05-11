52d10f6bde09b037d5216e3338a287d677e0467b
honeychest <scollo1982@gmail.com>
2026-05-12 00:17:42 +0900

    fix: 명시적 생성자 주입으로 aggTrade 스트림 빈 생성

--- springboot/src/main/java/com/chs/springboot/domain/binance/service/AggTradeStreamService.java
+++ springboot/src/main/java/com/chs/springboot/domain/binance/service/AggTradeStreamService.java
@@ -7,6 +7,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -43,6 +44,7 @@ public class AggTradeStreamService {
     private final List<BinanceWebSocketStream> streams = new ArrayList<>();
     private volatile boolean connected = false;
 
+    @Autowired
     public AggTradeStreamService(AggTradeParser parser, List<AggTradeSink> sinks) {
         this(parser, sinks, BinanceWebSocketStream::new);
     }
